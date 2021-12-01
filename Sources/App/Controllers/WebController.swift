4593007f649c92edda9b396a2b5adad5c1b7d186
Christopher Alford <chrisvalford@me.com>
2021-12-01 11:05:33 +0100

    Fixed navigation, layout issues and missing content

--- Sources/App/Controllers/WebController.swift
+++ Sources/App/Controllers/WebController.swift
@@ -10,8 +10,11 @@ import Vapor
 final class WebController : RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.get("", use: home)
+        routes.get("about", use: about)
+        routes.get("contact", use: contact)
+        routes.get("register", use: register)
         routes.get("login", use: login)
-        routes.get("profile", use: profile)
+        routes.get("sbc", use: sbc)
     }
 
     func home(req: Request) throws -> EventLoopFuture<View> {
@@ -19,6 +22,22 @@ final class WebController : RouteCollection {
             title: "Home",
             description: "Demonstration of the light weight Skeleton framework.")
         return req.render("index", context)
+    }
+
+    func about(req: Request) throws -> EventLoopFuture<View> {
+//        let member = Member(id: UUID(),fullName: "Jane Doe", eMail: "janedoe@acme.com", joinedDate: nil, lastSeenDate: Date())
+        let context = BaseContext(
+            title: "About Skeleton",
+            description: "Skeleton is a light weight css framework.")
+        return req.render("about", context)
+    }
+
+    func contact(req: Request) throws -> EventLoopFuture<View> {
+//        let member = Member(id: UUID(),fullName: "Jane Doe", eMail: "janedoe@acme.com", joinedDate: nil, lastSeenDate: Date())
+        let context = BaseContext(
+            title: "Contact Skeleton",
+            description: "Contact Skeleton - We try to answer questions as soon as we can. Please use a valid e-mail address.")
+        return req.render("contact", context)
     }
 
     func login(req: Request) throws -> EventLoopFuture<View> {
@@ -29,16 +48,24 @@ final class WebController : RouteCollection {
         return req.render("login", context)
     }
 
-    func profile(req: Request) throws -> EventLoopFuture<View> {
+    func register(req: Request) throws -> EventLoopFuture<View> {
+//        let member = Member(id: UUID(),fullName: "Jane Doe", eMail: "janedoe@acme.com", joinedDate: nil, lastSeenDate: Date())
+        let context = BaseContext(
+            title: "Register",
+            description: "Join Skeleton for extra benefits")
+        return req.render("register", context)
+    }
+
+    func sbc(req: Request) throws -> EventLoopFuture<View> {
 //        guard let memberId = req.parameters.get("memberID", as: UUID.self ) else {
 //            throw Abort(.badRequest)
 //        }
         // fake member - usually we would query the database
 //        let member = Member(id: UUID(),fullName: "Jane Doe", eMail: "janedoe@acme.com", joinedDate: nil, lastSeenDate: Date())
         let context = BaseContext(
-            title: "Member profile",
-            description: "Example of dynamically generated URL")
-        return req.render("profile", context)
+            title: "Single Board Computers",
+            description: "A variety of todays single board computers")
+        return req.render("sbc", context)
     }
 
 }
